8ea011c6c975b6c0704c3799dc2f406bd8d0b83f
Matthew Hillman <mhillman@bomgar.com>
2017-07-14 13:52:44 -0500

    Fix ordering

--- app/Member.php
+++ app/Member.php
@@ -13,13 +13,17 @@ class Member extends Model
 	protected $appends = ['todays_order'];
 
 	public function payout() {
-		return $this->belongsTo('App\Payout');
+		return $this->belongsTo('App\Payout', 'payout_id');
 	}
 
 	public function getTodaysOrderAttribute() {
 		$origin = $this->payout->order_effective_at;
 		$startDate = new DateTime($origin);
 		$today = new DateTime;
+		$diff = $today->diff($startDate);
+		if ($diff->h > 0 || $diff->i > 0 || $diff->s > 0) {
+			$today->modify("+1 day");
+		}
 		$today->setTime((int)$startDate->format('H'), (int)$startDate->format('M'));
 		$days = $today->diff($startDate)->d;
 
